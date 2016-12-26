9da365ee210c755b05b162acf5a52b5368e51716
David Sun <ffstudios1@gmail.com>
2016-12-26 12:04:12 -0800

    added basic editor components

--- components/editor/editor.js
+++ components/editor/editor.js
@@ -1,20 +1,28 @@
 import React, { PropTypes } from 'react';
 import s from './editor.css';
 import EditorToolbar from './editorToolbar'
+import {Editor, EditorState} from 'draft-js';
 
-class Editor extends React.Component {
+class EditorComponent extends React.Component {
   constructor(props){
     super(props)
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
   }
 
   render() {
     const {} = this.props;
+    console.log(this.refs);
     return (
       <div className={`${s["editor-wrapper"]}`}>
         <EditorToolbar/>
+        <div className={`${s["editor-core-wrapper"]}`}>
+          <input type="text" placeholder="A great story awaits" className={`${s["editor-introduction"]}`}/>
+          <Editor placeholder="May the force be with you" editorState={this.state.editorState} onChange={this.onChange} />
+        </div>
       </div>
     )
   }
 }
 
-export default Editor;
+export default EditorComponent;
