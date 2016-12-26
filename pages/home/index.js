4f7360f055106d8cd7ec811ddf72045d816388df
David Sun <ffstudios1@gmail.com>
2016-12-25 18:40:10 -0800

    initial commit

---
+++ pages/home/index.js
@@ -0,0 +1,31 @@
+/**
+ * React Static Boilerplate
+ * https://github.com/kriasoft/react-static-boilerplate
+ *
+ * Copyright © 2015-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React, { PropTypes } from 'react';
+import s from './styles.css';
+
+class HomePage extends React.Component {
+
+  static propTypes = {
+  };
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+
+}
+
+export default HomePage;
